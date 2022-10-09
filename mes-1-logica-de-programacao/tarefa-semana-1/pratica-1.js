549651fdb1769917fd0e2193d97e11df0a0ea7cf
Joao França <francapedro10@gmail.com>
2022-10-09 09:50:20 -0300

    CONSEGUI USAR O TYPEOF DEIXANDO EM STRING

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -9,3 +9,9 @@ email = 'francapedro10@gmail.com'
 profissao = 'Autonomo'
 idade = 20
 sonho = 'conhecer Dubai'
+
+console.log(typeof(nome))
+console.log(typeof(email))
+console.log(typeof(profissao))
+console.log(typeof(idade))
+console.log(typeof(sonho))
