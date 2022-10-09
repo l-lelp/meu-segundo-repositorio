5c052a7639f22c56da6ae8eaafc7a9616c78a605
Joao França <francapedro10@gmail.com>
2022-10-09 17:45:09 -0300

    terminando a tarefa 5

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,15 @@
+let nome
+let email
+let profissao
+let idade
+let sonho
+let assunto
+
+nome = 'Joao Franca'
+email = 'francapedro10@gmail.com'
+profissao = 'Autonomo'
+idade = 20
+sonho = 'conhecer Dubai'
+assunto = `Olá, Joao Franca! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para conhecer Dubai. Confirma pra gente alguns dados? A sua idade é 20, você é autonomo e o seu email é francapedro10@gmail.com? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+
+console.log(assunto)
