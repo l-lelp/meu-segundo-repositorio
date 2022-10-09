c291986ca95ce15fd9187e68334588023ab88c93
Joao França <francapedro10@gmail.com>
2022-10-09 17:56:38 -0300

    multiplicando igual uma tabuada

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,23 @@
+let numero1 = 1 
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+let x = 123
+ 
+console.log(x * numero1) * 123 
+console.log(x * numero2) * 123 
+console.log(x * numero3) * 123 
+console.log(x * numero4) * 123 
+console.log(x * numero5) * 123 
+console.log(x * numero6) * 123 
+console.log(x * numero7) * 123 
+console.log(x * numero8) * 123 
+console.log(x * numero9) * 123 
+console.log(x * numero10) * 123 
+
