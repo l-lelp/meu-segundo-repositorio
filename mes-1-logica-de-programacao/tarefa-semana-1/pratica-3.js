92ebc599dce2aa7780216c5d484af219427419a7
Joao França <francapedro10@gmail.com>
2022-10-09 10:55:26 -0300

    finalmente conseguidiferenciar os dois

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,21 @@
+// Verificando se é uma string ou não
+
+let nome = 'Joao'
+let email = 'francapedro10@gmail.com'
+let profissão = 'Autonomo'
+let idade = 20
+let lugarQueQueroConhecer = 'Dubai'
+
+// Verificação
+
+const tipoNome = typeof(nome)
+const tipoEmail = typeof(email)
+const tipoProfissão = typeof(profissão)
+const tipoIdade = typeof(idade)
+const tipoLugarQueQueroConhecer = typeof(lugarQueQueroConhecer)
+
+console.log(typeof(nome))
+console.log(typeof(email))
+console.log(typeof(profissão))
+console.log(typeof(idade))
+console.log(typeof(lugarQueQueroConhecer))
