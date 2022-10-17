86221026254595c77eb49984e75b51e4cdc83a18
Joao França <francapedro10@gmail.com>
2022-10-17 20:44:09 -0300

    conversando por função

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,22 @@
+function saudandoComprador() {
+    console.log("Olá, senhor José!")
+}
+
+function complementoDoProduto(nomeDoProduto, enderecoDaEntrega, nomeDaPessoaQueRecebeu) {
+    const Carregador = nomeDoProduto
+    const Petrolina = enderecoDaEntrega
+    const José = nomeDaPessoaQueRecebeu
+    console.log("Fizemos a entrega do produto Carregador no endereço Petrolina com sucesso!")
+
+    console.log("O seu produto foi recebido por: José")
+    
+    console.log("Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.")
+}
+
+function despedida() {
+    console.log("Atenciosamente, João França")
+}
+
+saudandoComprador()
+complementoDoProduto()
+despedida()
