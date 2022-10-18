a3360aa805d916efaa9f758d8e41bbc13564944c
Joao França <francapedro10@gmail.com>
2022-10-18 13:05:01 -0300

    tentei

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -1,3 +1,15 @@
 // Par ou ímpar?
 
 
+
+function parOuImpar () { 
+    if (3 % 2 == 0) {
+        console.log("O número ${numero} é par")
+
+} else {
+    console.log("O número ${numero} é ímpar")
+}
+
+}
+
+parOuImpar()
