c8b3a956a6f4de4e4832a854b7a96fe5ecbedc54
Joao França <francapedro10@gmail.com>
2022-10-17 21:12:13 -0300

    acho que deu certo!

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-5.js
@@ -0,0 +1,17 @@
+// Função que calcula o desconto. (expressão de função)
+
+function calcularDescontos (valor, desconto) {
+    let descontoAplicado = desconto/100
+    descontoAplicado = descontoAplicado.toFixed(2)
+    
+    const valorFinal = valor*(1 - descontoAplicado)
+    return valorFinal
+}
+
+const descontoFinal = function(calcularDescontos) {
+    const valorFinal = valor*(1 - descontoFinal)
+let valorTotal = valor + desconto
+return valorTotal
+}
+
+console.log("Total : " + calcularDescontos(100, 10))
