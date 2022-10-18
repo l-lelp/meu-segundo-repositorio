76aed3203f1faecfa88b032b16941e64c41bb706
Joao França <francapedro10@gmail.com>
2022-10-18 13:48:42 -0300

    maior entre 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,11 @@
+// Qual é o maior de 3 números?
+
+function maiorNumero(x , y, z) {
+    const maiorNumero = x
+    const menorNumero = y
+    const maiorNumeroMaior = z
+
+ console.log(Math.max(x,y,z))
+}
+
+maiorNumero(-30, 10, -100)
