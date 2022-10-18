5d46504e2d81941f8afea63e5f05b707d43f80ee
Joao França <francapedro10@gmail.com>
2022-10-18 14:45:03 -0300

    dias da semana

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-12.js
@@ -0,0 +1,15 @@
+// Exibindo o dia da semana
+
+function diasDaSemana() {
+    const Segunda = 1
+    const Terça = 2
+    const Quarta = 3
+    const Quinta = 4
+    const Sexta = 5
+    const Sábado = 6
+    const Domingo = 7
+    const data = ({Segunda, Terça, Quarta, Quinta, Sexta, Sábado, Domingo})
+    console.log(data) 
+}
+
+diasDaSemana ()
