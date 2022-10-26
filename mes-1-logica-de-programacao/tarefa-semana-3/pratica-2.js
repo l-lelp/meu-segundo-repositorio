d5cb30a1995c7d8ace9aafd90f3a06af345d7db1
Joao França <francapedro10@gmail.com>
2022-10-25 22:02:56 -0300

    varios impares

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -0,0 +1,15 @@
+// Exibindo os números Ímpares // 
+
+let exibindoNumerosImpares = n => {
+    for (let i = 1; i <= n; i += 2)
+        console.log(i)
+}
+
+exibindoNumerosImpares(17)
+exibindoNumerosImpares(27)
+exibindoNumerosImpares(999)
+exibindoNumerosImpares(1433)
+exibindoNumerosImpares(43331)
+exibindoNumerosImpares(789)
+exibindoNumerosImpares(13)
+exibindoNumerosImpares(53)
